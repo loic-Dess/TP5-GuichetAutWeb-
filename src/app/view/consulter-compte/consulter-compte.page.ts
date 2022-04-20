decdb684785029d8e2a030924650d088b610db60
LoicDesserich <loic.desserich@hotmail.com>
2022-04-20 10:35:04 +0200

    Pop-up depot et retrait

--- src/app/view/consulter-compte/consulter-compte.page.ts
+++ src/app/view/consulter-compte/consulter-compte.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController, ModalController } from '@ionic/angular';
+import { DepotComponent } from '../depot/depot.component';
+import { RetraieComponent } from '../retraie/retraie.component';
 
 @Component({
   selector: 'app-consulter-compte',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConsulterComptePage implements OnInit {
 
-  constructor() { }
+  constructor(    
+    private alertController: AlertController,
+    private modalCtrl: ModalController,
+    private router: Router
+  ) {
+
+  }
 
   ngOnInit() {
   }
 
+  async depot(){
+    const modal = await this.modalCtrl.create({
+      component: DepotComponent,
+      // componentProps: {}
+    })
+
+    await modal.present();
+  }
+
+  async retrait(){
+    const modal = await this.modalCtrl.create({
+      component: RetraieComponent,
+      // componentProps: {}
+    })
+
+    await modal.present();
+  }
 }
--- src/app/view/depot/depot.component.ts
+++ src/app/view/depot/depot.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { AlertController, ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-depot',
@@ -6,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./depot.component.scss'],
 })
 export class DepotComponent implements OnInit {
-
-  constructor() { }
+  compte = new FormControl()
+  operation = new FormControl('', Validators.required)
+  solde = new FormControl('')
+  
+  constructor(
+    private modalctrl: ModalController, 
+    private alertController: AlertController
+  ) { }
 
   ngOnInit() {}
 
+  dismissModal(data = false){
+    this.modalctrl.dismiss(data);
+  }
+
+  async confirmOperation(){
+    let titre = "Validation du retrait" 
+    let sousTitre = "Opération réussi"
+    let message = "Votre retrait d'un montant de " + this.operation.value + " a bien été pris en compte. \n" + 
+    "Le solde de votre compte : " + this.compte.value + " est de " + this.solde.value
+
+    const popup = await this.alertController.create({
+      header: titre,
+      subHeader: sousTitre,
+      message: message,
+      buttons: [{
+        text:'Continuer',
+        handler: () => {
+          this.dismissModal();
+        }
+      }],
+    });
+
+    await popup.present();
+  }
 }
